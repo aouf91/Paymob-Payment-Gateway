bc0e97d651a0847929b20b8760fa9876d3a3cce0
aouf91 <mido1048166@gmail.com>
2024-10-14 03:39:52 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,7 @@
-import express from "express"
+import express from "express";
 import router from "./routes/index.js";
 import expressLayouts from 'express-ejs-layouts';
 import connect from './database/db.js';
-
-
-import * as path from 'path';
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const PORT = 3000 || process.env.PORT;
 const server = express();
@@ -19,12 +10,7 @@ server.use(express.static('public'));
 
 // view engine setup
 server.use(expressLayouts)
-
-
-server.set('layout', path.join(__dirname, '/views/layouts/base'))
-
-//server.set('views', path.join(__dirname, 'views'));
-
+server.set('layout', './layouts/base')
 server.set('view engine', 'ejs');
 
 connect();
