1ce524da61f49b573f7f4d9bc7ab545ead82b4d7
aouf91 <mido1048166@gmail.com>
2024-10-17 04:51:06 +0300

    Update checkout.js

--- src/routes/v1/checkout.js
+++ src/routes/v1/checkout.js
@@ -6,6 +6,22 @@ const server = express.Router();
 server.post("/", async (req, res, next) => {
   try {
     // Get the order_cart, billing_data, amount_cents from the request body
+req.body["amount_cents"]=370000
+req.body["billin_data"]={
+  firstName: 'Ali',
+  lastName: 'Saad',
+  email: 'ali@gmail.com',
+  phoneNumber: '01088888888',
+  street: 'Hhhhhh',
+  apartment: 'Jjjjjjjjj',
+  building: '5',
+  floor: '2',
+  city: 'Dam',
+  state: 'Egy',
+  postalCode: '76282',
+  country: 'Egypt'
+}
+
     const { order_cart, billing_data, amount_cents } = req.body;
 console.log(req.body)
 
