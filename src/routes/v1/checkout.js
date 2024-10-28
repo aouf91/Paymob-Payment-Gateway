3121d73837d9bfdc7a30d2c8774c16c6b05c2b57
aouf91 <mido1048166@gmail.com>
2024-10-28 04:33:53 +0300

    Update checkout.js

--- src/routes/v1/checkout.js
+++ src/routes/v1/checkout.js
@@ -4,50 +4,63 @@ import { pay } from "../../utils/checkout.js";
 const server = express.Router();
 
 server.post("/", async (req, res, next) => {
-  try {
-    // Get the order_cart, billing_data, amount_cents from the request body
-req.body= {
-  "order_cart": [
-    {
+  console.log(req.body)
+    
+var myHeaders = new Headers();
+
+
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  amount: 2400000,
+  currency: "EGP",
+  payment_methods: [4853344,4853272],
+  items: [
+        {
       "name": "iphone x",
-      "amount_cents": "2400000",
+      "amount": "2400000",
       "description": "iphone x 64GB",
       "quantity": "1"
-    }
+    },
   ],
-  "billing_data": {
-    "apartment": "803",
-    "email": "claudette09@exa.com",
-    "floor": "42",
-    "first_name": "Clifford",
-    "street": "Ethan Land",
-    "building": "8028",
-    "phone_number": "+86(8)9135210487",
-    "shipping_method": "PKG",
-    "postal_code": "01898",
-    "city": "Jaskolskiburgh",
-    "country": "CR",
-    "last_name": "Nicolas",
-    "state": "Utah"
+  billing_data: {
+    apartment: "6",
+    first_name: "Ammar",
+    last_name: "Sadek",
+    street: "938, Al-Jadeed Bldg",
+    building: "939",
+    phone_number: "+96824480228",
+    country: "OMN",
+    email: "AmmarSadek@gmail.com",
+    floor: "1",
+    state: "Alkhuwair",
   },
-  "amount_cents": "2400000"
-}
+  customer: {
+    first_name: "Ammar",
+    last_name: "Sadek",
+    email: "AmmarSadek@gmail.com",
+    extras: {
+      re: "22",
+    },
+  },
+  extras: {
+    ee: 22,
+  },
+});
 
-    const { order_cart, billing_data, amount_cents } = req.body;
+var requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow",
+};
 
+fetch("https://accept.paymob.com/v1/intention/", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
 
-    // get the payment token for this order
-    const token = await pay(order_cart, billing_data, amount_cents);
-
-    // create the payment link
-    const link = `https://accept.paymob.com/api/acceptance/iframes/874327?payment_token=${token}`;
-
-
-    // respond with the payment link
-    return res.status(200).json(link);
-  } catch (error) {
-    return res.status(400).json(error);
-  }
+   
 });
 
 export default server;
