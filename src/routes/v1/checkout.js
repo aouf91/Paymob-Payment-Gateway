597904102c7f8e4f71b01c72f897587382777cf1
aouf91 <mido1048166@gmail.com>
2024-10-28 04:51:36 +0300

    Update checkout.js

--- src/routes/v1/checkout.js
+++ src/routes/v1/checkout.js
@@ -4,41 +4,29 @@ import { pay } from "../../utils/checkout.js";
 const server = express.Router();
 
 server.post("/", async (req, res, next) => {
-  console.log(req.body)
-    
 var myHeaders = new Headers();
-
-
+myHeaders.append(
+  "Authorization",process.env.SECRET_KEY
+);
 myHeaders.append("Content-Type", "application/json");
 
 var raw = JSON.stringify({
-  amount: 2400000,
+  amount: 24000,
   currency: "EGP",
   payment_methods: [4853344,4853272],
   items: [
         {
       "name": "iphone x",
-      "amount": "2400000",
+      "amount": "24000",
       "description": "iphone x 64GB",
       "quantity": "1"
     },
   ],
-  billing_data: {
-    apartment: "6",
-    first_name: "Ammar",
-    last_name: "Sadek",
-    street: "938, Al-Jadeed Bldg",
-    building: "939",
-    phone_number: "+96824480228",
-    country: "OMN",
-    email: "AmmarSadek@gmail.com",
-    floor: "1",
-    state: "Alkhuwair",
-  },
+  billing_data: req.body,
   customer: {
-    first_name: "Ammar",
-    last_name: "Sadek",
-    email: "AmmarSadek@gmail.com",
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
     extras: {
       re: "22",
     },
